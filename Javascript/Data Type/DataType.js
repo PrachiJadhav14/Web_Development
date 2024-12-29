f20b44436588f69d63a9c4efda520b108cf9325a
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-12-29 14:12:56 +0530

    Datatype

---
+++ Javascript/Data Type/DataType.js
@@ -0,0 +1,27 @@
+// Primary Datatypes / Primitive
+
+// 1. Number
+//      e.g - 1.,70,40,56,-1,Infinity
+
+// 2. String
+//     e.g  - 'WEB', "1001", '10.25', 'true,','A'
+
+// 3.Boolean
+//     e.g - true, false
+
+// 4.BigInt
+//      e.g - var x = BigInt ('85536536676353555777');
+
+// 5.Undefined
+//      e.g - var x;
+//         - Undefined
+
+// 6.Null 
+//     e.g - Var x = null;
+//            typeof(x);
+
+
+// Non Primary / Object Typed
+
+//  1. Array - []
+//  2.Object - {}
