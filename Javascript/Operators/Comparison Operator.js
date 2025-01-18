ea54bbffc71194c07542e42d597af904b79b8dca
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-01-18 21:49:45 +0530

    Comparison_Operator

---
+++ Javascript/Operators/Comparison Operator.js
@@ -0,0 +1,13 @@
+<script>
+        //  Strict Equality
+        // var x = 0;
+        // var y = false;
+        //  console.log(x==y);
+
+        // Strictly Not Equal
+        console.log( 1 !== 1);
+        console.log("1" !== 1);
+        console.log(0 !== false);
+        console.log("true" !== 1);
+        
+</script>
