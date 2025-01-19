0abccaeda87d7ec4522469216588d2ef28004581
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-01-19 21:54:52 +0530

    Turnary

--- Javascript/Statements/Turnary.js
+++ Javascript/Statements/Turnary.js
@@ -1,7 +1,6 @@
-<script>
+
 
     var StudentMarks=29;
     let Result = StudentMarks >=70 && StudentMarks <= 95 ? "Grade A" : StudentMarks >=55 && StudentMarks <= 69 ? "Grade B" : StudentMarks >=30 && StudentMarks <= 54 ? "Grade C" : "Fail";
     console.log(Result);
     
-</script>
