423170125f71db5889fdbc8af692a3c01a7be1cb
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-01-25 21:45:40 +0530

    Arithematic_Operator

--- Javascript/Operators/Arithematic Operator.js
+++ Javascript/Operators/Arithematic Operator.js
@@ -1,35 +1,33 @@
 <script>
-  var x = 10;    //Base Value
-  var y = 3;      ////Power Value
+  var x = 10;    {/*Base Value */}
+  var y = 3;      {/*Power Value*/}
   console.log(x ** y);
   console.log(Math.pow(10,3));
 
-  // Increment Operator
-  // Post Increment
+  {/* // Increment Operator
+  // Post Increment */}
   var x = 10;
   console.log(x++);  //10
   var y = x++;
   console.log(y);    //11
 
-  //// Pre Increment
+  {/* //// Pre Increment */}
   //   var x = 20;
   //  console.log(++x); // 21
   //  console.log(x);  /// 22
 
-
+{/* 
   //Decrement Operator
-  ////Post Increment
+  ////Post Increment */}
   var x = 10;
   var y = x--;   // y = 10, x = 9
   console.log(x);  // 9
   console.log(y);  // 10
 
 
-  //   Pre Decrement
+  {/* //   Pre Decrement */}
   var a = 10;
   let b = --a;  //b= 9 , a = 9
   console.log(a); //9
   console.log(b);  //9
-
-
 </script>
