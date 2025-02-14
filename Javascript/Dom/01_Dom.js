37c2b69de18c9e1048ca41d812881fcca598af87
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-02-14 21:26:33 +0530

    DOM_Js

--- Javascript/Dom/01_Dom.js
+++ Javascript/Dom/01_Dom.js
@@ -1,4 +1,3 @@
-
 //   Dom Manupilation
 // let heading= document.getElementById("heading");
 // console.dir(heading);
@@ -14,24 +13,18 @@
 //    console.dir(firstele); let first= document.querySelectorAll("p");c
 //    console.log(firstele);
 
-
 //    let firstle = document.querySelector("p");   //1st Element
 //    console.dir(firstle);
 //    console.log(firstle);
-
 
 //    let Element = document.querySelectorAll("p");   ///All Element
 //    console.dir(Element);
 //    console.log(Element);
 
+let Element = document.querySelector(".heading2");
+console.dir(Element);
+console.log(Element);
 
- let Element = document.querySelector(".heading2");
-   console.dir(Element);
-   console.log(Element);
-
-   
-
-   
-   let Button = document.querySelector("#button");
-   console.dir(Button);
-   console.log(Button);
+let Button = document.querySelector("#button");
+console.dir(Button);
+console.log(Button);
