3f097cced9264eae078f70890cf568841b6a561d
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-02-16 11:54:04 +0530

    js_File

--- Javascript/Dom/Practice_Set/Practice_Set.js
+++ Javascript/Dom/Practice_Set/Practice_Set.js
@@ -3,12 +3,21 @@
 
 // h2.innerText = h2.innerText + "From Apna College Student";
 
-// let divs = document.querySelectorAll(".box");
+let divs = document.querySelectorAll(".box");
+
+for(div of divs){
+    console.log(div.innerText);
+}
 
 
-// let idx = 1;
-// for (div of divs){
-//     div.innerText = `New unique value ${idx}`;
-//     idx++;
-// }
-console.log("Hello ");
+// divs[0].innerText = "New unique value 1";
+// divs[1].innerText = "New unique Value 2";
+// divs[2].innerText = "New unique Value 3";
+
+
+
+let idx = 1;
+for (div of divs){
+    div.innerText = `New unique value ${idx}`;
+    idx++;
+}
