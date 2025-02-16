66fbdb6a35cbe03ff517227da0be6b514724980d
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-02-16 11:54:36 +0530

    Practice_Set_Dom

--- Javascript/Dom/Practice_Set/Practice_Set.js
+++ Javascript/Dom/Practice_Set/Practice_Set.js
@@ -8,13 +8,9 @@ let divs = document.querySelectorAll(".box");
 for(div of divs){
     console.log(div.innerText);
 }
-
-
 // divs[0].innerText = "New unique value 1";
 // divs[1].innerText = "New unique Value 2";
 // divs[2].innerText = "New unique Value 3";
-
-
 
 let idx = 1;
 for (div of divs){
