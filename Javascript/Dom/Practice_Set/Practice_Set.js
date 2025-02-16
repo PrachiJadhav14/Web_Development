bdfb1ee701178c196750cabf0c9d9430ba74072a
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-02-16 11:05:05 +0530

    Practice_Set.js

---
+++ Javascript/Dom/Practice_Set/Practice_Set.js
@@ -0,0 +1,14 @@
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + "From Apna College Student";
+
+// let divs = document.querySelectorAll(".box");
+
+
+// let idx = 1;
+// for (div of divs){
+//     div.innerText = `New unique value ${idx}`;
+//     idx++;
+// }
+console.log("Hello ");
