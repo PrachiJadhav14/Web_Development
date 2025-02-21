4f8897bcd59cf24064486c173b46da28b26e3a4e
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-02-21 12:40:51 +0530

    DOM_Events.js

--- Javascript/Dom/DOM_Event.js
+++ Javascript/Dom/DOM_Event.js
@@ -1,16 +1,19 @@
 let btn1 = document.querySelector("#btn1");
 
+
+///EVENT_DOM_OBJECT
+
 // btn1.onclick = (evt) => {
 //     console.log(evt);
 //     console.log(evt.type);            //Type         
 //     console.log(evt.target);         ///Target
 //     console.log(evt.clientX, evt.clientY);
 
-    // console.log("btn1 Was Clicked");
-    // let a = 25;
-    // a++;
-    // console.log(a);
-}
+//     console.log("btn1 Was Clicked");
+//     let a = 25;
+//     a++;
+//     console.log(a);
+// }
 
 let div = document.querySelector("div");
 div.onmousedown = (evt) => {
