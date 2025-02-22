fde1c3d5a4c7f21415941c755c4c5bfb6657c947
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-02-22 12:43:15 +0530

    Event_Practice_Set

---
+++ Javascript/Dom/Event_DOM/Event_Practice_Set.js
@@ -0,0 +1,15 @@
+let modBtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currMode = "light"; //dark
+
+modBtn.addEventListener("click",() => {
+    if (currMode === "light"){
+        currMode = "dark";
+        body.classList.add("dark");
+    }else{
+        currMode = "light";
+       body.classList.add("light")
+    }
+     
+    console.log(currMode);
+});
