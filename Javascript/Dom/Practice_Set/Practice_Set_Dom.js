f4976562a7aae1928950e508e7e247aaaa1b0286
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-02-24 16:46:00 +0530

    Practice_Set_DOm

---
+++ Javascript/Dom/Practice_Set/Practice_Set_Dom.js
@@ -0,0 +1,41 @@
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + "From Apna College Student";
+
+// let divs = document.querySelectorAll(".box");
+
+// for(div of divs){
+//     console.log(div.innerText);
+// }
+// // divs[0].innerText = "New unique value 1";
+// // divs[1].innerText = "New unique Value 2";
+// // divs[2].innerText = "New unique Value 3";
+
+// let idx = 1;
+// for (div of divs){
+//     div.innerText = `New unique value ${idx}`;
+//     idx++;
+// }
+
+
+
+
+// practice Set 2)
+// Q1)
+
+let newbtn = document.createElement("button");
+newbtn.innerText = "Click Me !";
+
+
+
+newbtn.style.color = "White";
+newbtn.style.backgroundColor = "red";
+
+document.querySelector("body").prepend(newbtn);
+
+// Q2)
+ 
+let para = document.querySelector("p");
+
+
