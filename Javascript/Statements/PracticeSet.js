49c5ed62e2353132e352832ecd9a894ff07f778a
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-06-06 23:44:53 +0530

    Practice_Set

--- Javascript/Statements/PracticeSet.js
+++ Javascript/Statements/PracticeSet.js
@@ -3,8 +3,10 @@
     let num = prompt("Enter a Number:"); 
     if (num % 5 === 0)
     {
-    console.log(num,"is Muliple Of 5");
+
+           console.log(num,"is Muliple Of 5");
     }
+
     else
     {
         console.log(num,"is Not Multiple Of 5");
