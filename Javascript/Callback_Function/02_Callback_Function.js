5ab39a196254ea1d468c32c10d147611593f3897
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-08-21 14:22:55 +0530

    Callback_Function

---
+++ Javascript/Callback_Function/02_Callback_Function.js
@@ -0,0 +1,60 @@
+
+ function Display(X){
+  
+       console.log("Value Of X :",X);
+    }
+
+    function sum(a,b,sendFunction){
+
+        let sum = a + b;
+        sendFunction(sum);
+        
+    }
+
+
+    sum(10,60,Display);
+
+
+
+//     2)
+    
+//    function Display(X){
+  
+//        console.log("Value Of X :",X);
+//     }
+
+//     function sum(a,b,sendFunction){
+
+//         let sum = a + b;
+//         sendFunction(sum);
+        
+//     }
+
+
+//     sum(10,60,Display);
+
+
+
+
+
+
+
+//3)
+    // function Display(X){
+    //     console.log("Inside Function ");
+
+    // }
+
+    // function fun(y){
+    //     console.log("Inside fun",y);
+    //     Display();
+
+    // }
+
+    // function sum(a,b,sendFunction){
+    //     let sum = a + b;
+    //     sendFunction(sum) 
+    // }
+
+    // sum(10,20,fun);
+
