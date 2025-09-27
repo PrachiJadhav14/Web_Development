fd2c48c85f32a51fd3007e3c2f9bcf9f777094bd
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-09-27 23:07:01 +0530

    Revision

---
+++ Javascript/Revision/Function/01_Function.js
@@ -0,0 +1,5 @@
+function greet(name){
+    return "Hellow " + name;
+}
+let myName = "Prachi";
+console.log(greet(myName));
---
+++ Javascript/Revision/Objetc/01_Object.js
@@ -0,0 +1,21 @@
+let person = {
+    name : "Prachi",
+    Age : 20,
+    city: "pune",
+    isStudent:true
+};
+
+console.log(person);
+// console.log(typeof(person));
+//  console.log(typeof(person.name));
+
+ let name = person.name;
+
+console.log(name);
+
+let Age = person.Age;
+ console.log(Age);
+
+
+let num = [10,20];
+console.log(num);
