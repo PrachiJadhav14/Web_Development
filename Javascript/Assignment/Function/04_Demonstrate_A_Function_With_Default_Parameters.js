7379b783e313fdf3d063ef89ea815b6380d05e30
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-10-07 17:42:20 +0530

    Function

---
+++ Javascript/Assignment/Function/04_Demonstrate_A_Function_With_Default_Parameters.js
@@ -0,0 +1,7 @@
+function defaultMessage(name = "Guest",Message = "Welcome To Our Javascript"){
+    console.log(`Hellow ${name}, ${Message}`);
+};
+
+defaultMessage();
+defaultMessage("Prachi");
+defaultMessage("Prachi","Have A Great Day");
