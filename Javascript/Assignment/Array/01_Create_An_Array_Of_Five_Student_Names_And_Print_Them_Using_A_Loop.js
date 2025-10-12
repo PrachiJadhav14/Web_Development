918a2ecda7020726950a18b29ab921a308891e53
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-10-12 15:43:27 +0530

    Array

---
+++ Javascript/Assignment/Array/01_Create_An_Array_Of_Five_Student_Names_And_Print_Them_Using_A_Loop.js
@@ -0,0 +1,7 @@
+
+var Student = ["Prachi","Sakshi","Sneha","Shiv","saku"];
+
+
+for(let i = 0; i<Student.length;i++){
+    console.log(Student[i]);
+}
---
+++ Javascript/Assignment/Array/03_Remove_duplicate_values_from_an_array.js
@@ -0,0 +1,16 @@
+// Remove duplicate values from an array (hint: use Set).
+
+let Numbers = [1,2,3,4,2,3,1];
+
+let myset = new set(Numbers);
+
+console.log(myset);
+
+
+
+// let numbers = [1, 2, 3, 2, 4, 1, 5];
+
+// // Using Set to remove duplicates
+// let uniqueNumbers = [...new Set(numbers)];
+
+// console.log(uniqueNumbers);
