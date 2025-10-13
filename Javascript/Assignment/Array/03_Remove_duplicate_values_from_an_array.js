a69473647abd3970c5024827d153828427e54893
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-10-13 15:52:03 +0530

    Remove_Duplicate_Array

--- Javascript/Assignment/Array/03_Remove_duplicate_values_from_an_array.js
+++ Javascript/Assignment/Array/03_Remove_duplicate_values_from_an_array.js
@@ -2,15 +2,17 @@
 
 let Numbers = [1,2,3,4,2,3,1];
 
-let myset = new set(Numbers);
+ let mySet = new Set(Numbers);
 
-console.log(myset);
+console.log(mySet);
 
 
 
-// let numbers = [1, 2, 3, 2, 4, 1, 5];
 
-// // Using Set to remove duplicates
-// let uniqueNumbers = [...new Set(numbers)];
 
-// console.log(uniqueNumbers);
+ let numbers = [1, 2, 3, 2, 4, 1, 5];
+
+
+let uniqueNumbers = [...new Set(numbers)];
+
+console.log(uniqueNumbers);
