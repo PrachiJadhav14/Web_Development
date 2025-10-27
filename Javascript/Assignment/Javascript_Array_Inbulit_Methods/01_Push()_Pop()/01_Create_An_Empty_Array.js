1cfa428bade6f63e836d2e29668fa52e667d176e
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-10-27 16:02:12 +0530

    Push()&Pop()_Methos

---
+++ Javascript/Assignment/Javascript_Array_Inbulit_Methods/01_Push()_Pop()/01_Create_An_Empty_Array.js
@@ -0,0 +1,11 @@
+//Create An Empty Array Use Push () To Add 5 Student Names into it 
+
+var Student = [];
+
+Student.push("Prachi");
+Student.push("Saku");
+Student.push("Rutu");
+Student.push("Aadu");
+Student.push("Ruchi");
+
+console.log(Student);
