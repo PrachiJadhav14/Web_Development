25f73aa2cf9bcab1f80116e690cb9684bbb2766b
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-11-13 19:52:29 +0530

    Operator

--- Javascript/Assignment/Operator/02_Compare_Two_Numbers_Using_Relational_Operators.js
+++ Javascript/Assignment/Operator/02_Compare_Two_Numbers_Using_Relational_Operators.js
@@ -1,15 +1,14 @@
 let p = 60;
 let q = 50;
-console.log(p>q);
+console.log(p > q);
 
 let a = 20;
 let b = 10;
-console.log(a<b);
+console.log(a < b);
 
 let myAge = 20;
 let myfreindage = 20;
 console.log(myAge == myfreindage);
-
 
 let MyAge = 20;
 let Friendage = 25;
@@ -17,4 +16,4 @@ console.log(MyAge === Friendage);
 
 let marks = 70;
 let passmarks = 35;
-console.log(marks != passmarks); 
+console.log(marks != passmarks);
